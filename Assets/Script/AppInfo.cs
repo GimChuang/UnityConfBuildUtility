cbe4a5180b5927925c0cc8eef3f63c596428a381
gim_razer <rgm79ms@gmail.com>
2018-12-04 10:23:27 +0800

    Added function to overwrite AppInfoData json file to update buildCount

--- Assets/Script/AppInfo.cs
+++ Assets/Script/AppInfo.cs
@@ -10,46 +10,100 @@ public class AppInfo {
     public string ProductName;
     public bool bForceSingleInstance;
 
-    public int buildDate;
+    public string buildDate;
     public int buildCount;
+
+    string currentDateString;
 
     public AppInfo(string _appInfoDataPath, string _productName, bool _bForceInstance)
     {
-        LoadAppInfoData(_appInfoDataPath);
+        
+        currentDateString = string.Format("{0:yyMMdd}", System.DateTime.Now);
+
+        // Load data from json to set the buildDate and buildCount
+        AppInfoData data = LoadAppInfoData(_appInfoDataPath);
+        if (data != null)
+        {
+            if (data.buildDate != currentDateString)
+            {
+                data.buildDate = currentDateString;
+                data.buildCount = 1;
+            }
+            else
+                data.buildCount++;
+
+            Debug.Log("AppInfo: " + data.buildDate + "_" + data.buildCount);
+
+            buildDate = data.buildDate;
+            buildCount = data.buildCount;
+
+            // Overwrite the file to update buildCount!
+            UpdateAppInfoData(_appInfoDataPath, data);
+        }
+        else
+        {
+            Debug.LogError("Something's wrong when reading the AppInfoData json file");
+        }
 
         ProductName = _productName;
-        bForceSingleInstance = _bForceInstance;        
+        bForceSingleInstance = _bForceInstance;
+        
     }
 
-    public void LoadAppInfoData(string _path)
+    public AppInfoData LoadAppInfoData(string _appInfoDataPath)
+    {
+        // If the file does not exist, create a new one
+        if (!File.Exists(_appInfoDataPath))
+        {
+            string stringToWrite = "{" + "\"buildDate\":" + "\"" + currentDateString + "\"" + "," + "\"buildCount\":" + "0" + "}";
+            using (FileStream fs = new FileStream(_appInfoDataPath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.Write(stringToWrite);
+                    writer.Close();
+                    writer.Dispose();
+                }
+                fs.Close();
+                fs.Dispose();
+            }
+
+            Debug.LogWarning("AppInfoData file not exists. Created one.");
+        }
+
+        try
+        {
+            string jsonString = File.ReadAllText(_appInfoDataPath);
+            AppInfoData data = JsonUtility.FromJson<AppInfoData>(jsonString);
+            Debug.Log("AppInfoData Loaded.");
+            return data;
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+            return null;
+        }
+    }
+
+    public void UpdateAppInfoData(string _appInfoDataPath, AppInfoData _appInfoData)
     {
         try
         {
-            string jsonString = File.ReadAllText(_path);
-            AppInfoData appInfoData = JsonUtility.FromJson<AppInfoData>(jsonString);
-
-            Debug.Log("AppInfoData Loaded.");
-
-            buildDate = appInfoData.buildDate;
-            buildCount = appInfoData.buildCount;
-
+            JsonData dataToSave = JsonMapper.ToJson(_appInfoData);
+            File.WriteAllText(_appInfoDataPath, dataToSave.ToString());
+            Debug.Log("Overwrote AppInfoData.");
         }
         catch (Exception e)
         {
             Debug.LogError(e.Message);
         }
     }
-
-    public void UpdateAppInfoData(string _date, int _count)
-    {
-
-    }
 }
 
 [Serializable]
 public class AppInfoData
 {
-    public int buildDate;
+    public string buildDate;
     public int buildCount;
 }
 
